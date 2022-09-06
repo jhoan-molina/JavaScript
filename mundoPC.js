6b31f69bfeddc9ba7228f3cb027d076032ec6e54
jhoan-molina <56314073+jhoan-molina@users.noreply.github.com>
2022-09-05 22:56:23 -0500

    Add files via upload

---
+++ mundoPC.js
@@ -0,0 +1,150 @@
+class DispositivoEntrada{
+    constructor(tipoEntrada, marca){
+        this._tipoEntrada = tipoEntrada;
+        this._marca = marca;
+    }
+
+    get tipoEntrada(){
+        return this._tipoEntrada;
+    }
+
+    set tipoEntrada(tipoEntrada){
+        this._tipoEntrada = tipoEntrada;
+    }
+
+    get marca(){
+        return this._marca;
+    }
+
+    set marca(marca){
+        this._marca = marca;
+    }
+
+    toString(){
+        return `Entrada: ${this._tipoEntrada} Marca: ${this._marca}`;
+    }
+}
+
+class Raton extends DispositivoEntrada{
+    static contadorRatones = 0;
+
+    constructor(tipoEntrada, marca){
+        super(tipoEntrada,marca);
+        this._idRaton = ++Raton.contadorRatones;
+    }
+
+    get idRaton(){
+        return this._idRaton;
+    }
+
+    toString(){
+        return `Raton: [idRaton: ${this._idRaton} ${super.toString()}]`;
+    }
+}
+
+class Teclado extends DispositivoEntrada{
+
+    static contadorTeclados = 0;
+
+    constructor(tipoEntrada, marca){
+        super(tipoEntrada,marca);
+        this._idTeclado = ++Teclado.contadorTeclados;
+    }
+
+    get idTeclado(){
+        return this._idTeclado;
+    }
+
+    toString(){
+        return `Teclado: [idTeclado: ${this._idTeclado} ${super.toString()}]`;
+    }
+}
+
+class Monitor{
+    static contadorMonitores = 0;
+
+    constructor(marca,tamaño){
+        this._idMonitor = ++Monitor.contadorMonitores;
+        this._marca = marca;
+        this._tamaño = tamaño;
+    }
+
+    get idMonitor(){
+        return this._idMonitor;
+    }
+
+    get marca(){
+        return this._marca;
+    }
+
+    set marca(marca){
+        this._marca = marca;
+    }
+
+    get tamaño(){
+        return this._tamaño;
+    }
+
+    set tamaño(tamaño){
+        this._tamaño=tamaño;
+    }
+
+    toString(){
+        return `Monitor: [idMonitor: ${this._idMonitor} Marca: ${this._marca} Tamaño: ${this._tamaño}]`;
+    }
+}
+
+class Computadora{
+    static contadorComputadoras = 0;
+
+    constructor(nombre, monitor, teclado, raton){
+        this._idComputadora = ++Computadora.contadorComputadoras;
+        this._nombre = nombre;
+        this._monitor = monitor;
+        this._teclado = teclado;
+        this._raton = raton;
+    }
+
+    toString(){
+        return `Computadora ${this._idComputadora}:  ${this._nombre} \n ${this._monitor} \n ${this._raton} \n ${this._teclado}`;
+    }
+}
+
+class Orden{
+    static contadorOrdenes = 0;
+
+    constructor(){
+        this._idOrden = ++Orden.contadorOrdenes;
+        this._computadoras = [];
+    }
+
+    get idOrden(){
+        return this._idOrden;
+    }
+
+    agregarComputadora(computadora){
+        this._computadoras.push(computadora);
+    }
+
+    mostrarOrden(){
+        let computadorasOrden = '';
+        for(let computadora of this._computadoras){
+            computadorasOrden += '\n'+computadora.toString();
+        }
+        console.log(`Orden: ${this._idOrden}, Computadoras: ${computadorasOrden}`);
+    }
+}
+
+let raton1 = new Raton('USB', 'Logitech');
+let teclado1 = new Teclado('USB', 'Logitech');
+let monitor1 = new Monitor('LG', '35 Pulgadas');
+let computadora1 = new Computadora('HP', monitor1, teclado1, raton1);
+let raton2 = new Raton('Bluetooth', 'Dell');
+let teclado2 = new Teclado('USB', 'Acer');
+let monitor2 = new Monitor('Dell', '27 Pulgadas');
+let computadora2 = new Computadora('Armada', monitor2, teclado2, raton2);
+let orden1 = new Orden();
+orden1.agregarComputadora(computadora1);
+orden1.agregarComputadora(computadora2);
+orden1.mostrarOrden();
+
